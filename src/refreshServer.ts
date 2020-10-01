d0bc7d10a84b57ac3f097a8e4283041ad978a46e
Ville Nuutinen <hunttis@Villes-iMac.local>
2020-10-01 16:17:11 +0300

    Fixed build command, Added build script, added favicon

--- src/refreshServer.ts
+++ src/refreshServer.ts
@@ -1,7 +1,9 @@
-export const evtSource = new EventSource(
-  "http://localhost:8321/refresh-events/SceneA.html"
-);
+export function listenRefresh(file: string) {
+  const evtSource = new EventSource(
+    `http://localhost:8321/refresh-events/${file}`
+  );
 
-evtSource.addEventListener("refresh", () => {
-  window.location.reload();
-});
+  evtSource.addEventListener("refresh", () => {
+    window.location.reload();
+  });
+}
